982f46a210c39e2bf91965dffb82497834aa9c5e
liyongbing <liyongbing@163.com>
2017-03-03 11:50:23 +0800

    config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+//define("CONFIG_ROOT_DICTORY", "E:/www/20160401/");
+define("CONFIG_ROOT_DICTORY", str_replace("\\",'/',getcwd().'/'));
+define("CONFIG_DEFAULT_HOME", "/public/pub/index");
+/*define("CONFIG_DB_HOST", "rds4w9y5usaa5l4ir24iw.mysql.rds.aliyuncs.com");
+define("CONFIG_DB_USER", "planaman");*/
+define("CONFIG_DB_HOST", "localhost");
+define("CONFIG_DB_USER", "root");
+define("CONFIG_DB_PASS", "root");
+define("CONFIG_DB_PORT", "3306");
+/*define("CONFIG_DB_PASS", "newyunadmplan20150611datadm");*/
+define("CONFIG_DB_NAME", "plana");
+define("CONFIG_DB_SAIKU", "saiku");//saiku位置名字
+define("CONFIG_SMARTY_COMPILECHECK", true);  
+define('YEARS', "2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015");
+?>
