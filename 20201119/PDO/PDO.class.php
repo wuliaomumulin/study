c3437963f8ce40f1e5deb3b922ec3eb50cb67de4
wuliaomumu <761071654@qq.com>
2020-11-20 14:12:27 +0800

    Update PDO.class.php

--- 20201119/PDO/PDO.class.php
+++ 20201119/PDO/PDO.class.php
@@ -9,10 +9,12 @@
 		protected $dbName = '';
 		protected $dsn;
 		protected $dbh;
+		//是否开启debug模式
+		public $debug = false;
 		/**
 		 * 构造函数
 		 */
-		private function __construct($dbHost,$dbUser,$dbPasswd,$dbName,$dbCharset){
+		public function __construct($dbHost,$dbUser,$dbPasswd,$dbName,$dbCharset){
 			try{
 				$this->dsn = 'mysql:host='.$dbHost.';dbname='.$dbName;
 				$this->dbh = new PDO($this->dsn,$dbUser,$dbPasswd);
@@ -44,11 +46,10 @@
 		 * [query description]
 		 * @param  [string]  $sqlstr    [查询字符串]
 		 * @param  string  $querymode [查询模式，All:全部|Row：单条]
-		 * @param  boolean $debug     [是否返回sql语句，默认false]
 		 * @return [array]    $result         [数据]
 		 */
-		public function query($sqlstr,$querymode='All',$debug=false){
-				if($debug==true) $this->debug($sqlstr);
+		public function query($sqlstr,$querymode='All'){
+				if($this->debug==true) return $sqlstr;
 				$recordset = $this->dbh->query($sqlstr);
 				$this->getPDOError();
 				if($recordset){
@@ -68,10 +69,9 @@
 		 * @param  [type] $table          [表名]
 		 * @param  [type] $arrayDataValue [数据]
 		 * @param  string $where          [条件]
-		 * @param  [type] $debug          [错误输出]
 		 * @return [type]                 [description]
 		 */
-		public function update($table,$arrayDataValue,$where='',$debug=false){
+		public function update($table,$arrayDataValue,$where=''){
 			$this->checkFields($table,$arrayDataValue);
 			if($where){
 				$strSql = '';
@@ -79,63 +79,87 @@
 					$strSql.=','.$key.'="'.$val.'"';
 				}
 				$strSql = substr($strSql,1);
-				$strSql = 'update '.$table.' set '.$strSql.' where '.$where;
+				$strSql = 'update `'.$table.'` set '.$strSql.' where '.$where;
 			}else{
-				$strSql = 'replace into '.$table.'('.implode(',',array_keys($arrayDataValue)).') values("'.implode('","',$arrayDataValue).'")';
+				$strSql = 'replace into `'.$table.'`('.implode(',',array_keys($arrayDataValue)).') values("'.implode('","',$arrayDataValue).'")';
 			}
-			$result = $this->execSql($strSql,$debug);
+			$result = $this->execSql($strSql);
 			return $result;
 		}
 		/**
 		 * [insert 插入]
 		 * @param  [type]  $table          [表名]
 		 * @param  [type]  $arrayDataValue [数据]
-		 * @param  boolean $debug          [调试]
 		 * @return [type]                  [description]
 		 */
-		public function insert($table,$arrayDataValue,$debug=false){
-			$this->checkFields($table,$arrayDataValue);
-			$strSql = 'insert into '.$table.'('.implode(',',array_keys($arrayDataValue)).') values("'.implode('","',$arrayDataValue).'")';
-			$result = $this->execSql($strSql,$debug);
+		public function insert($table,$arrayDataValue){
+			$this->checkFields($table,$arrayDataValue);			
+			$strSql = 'insert into `'.$table.'`('.implode(',',array_keys($arrayDataValue)).') values("'.implode('","',$arrayDataValue).'")';
+			$result = $this->execSql($strSql);
+			return $result;
+		}
+		/**
+		 * [insert 插入一簇]
+		 * @param  [type]  $table          [表名]
+		 * @param  [type]  $arrayDataValue [数据]
+		 * @return [type]                  [description]
+		 */
+		public function insertCluster($table,$arrayDataValue){
+			$this->checkFields($table,$arrayDataValue[0]);
+			$val = '';
+			foreach ($arrayDataValue as $arr) $val .= '("'.implode('","',$arr).'"),';
+			$strSql = 'insert into `'.$table.'`('.implode(',',array_keys($arrayDataValue[0])).') values'.rtrim($val,',');
+			$result = $this->execSql($strSql);
 			return $result;
 		}
 		/**
 		 * [replace 覆盖,需有主键ID]
 		 * @param  [type]  $table     
-		 * @param  [type]  $arrayDataValue 
-		 * @param  boolean $debug       
+		 * @param  [type]  $arrayDataValue      
 		 * @return [type]                  
 		 */
-		public function replace($table,$arrayDataValue,$debug=false){
+		public function replace($table,$arrayDataValue){
 			$this->checkFields($table,$arrayDataValue);
-			$strSql = 'replace into '.$table.'('.implode(',',array_keys($arrayDataValue)).') values("'.implode('","',$arrayDataValue).'")';
-			$result = $this->execSql($strSql,$debug);
+			$strSql = 'replace into `'.$table.'`('.implode(',',array_keys($arrayDataValue)).') values("'.implode('","',$arrayDataValue).'")';
+			$result = $this->execSql($strSql);
+			return $result;
+		}
+		/**
+		 * [insert 覆盖一簇,需有主键ID]
+		 * @param  [type]  $table          [表名]
+		 * @param  [type]  $arrayDataValue [数据]
+		 * @return [type]                  [description]
+		 */
+		public function replaceCluster($table,$arrayDataValue){
+			$this->checkFields($table,$arrayDataValue[0]);
+			$val = '';
+			foreach ($arrayDataValue as $arr) $val .= '("'.implode('","',$arr).'"),';
+			$strSql = 'replace into `'.$table.'`('.implode(',',array_keys($arrayDataValue[0])).') values'.rtrim($val,',');
+			$result = $this->execSql($strSql);
 			return $result;
 		}
 		/**
 		 * [delete 删除]
 		 * @param  [string]  $table 
 		 * @param  string  $where 
-		 * @param  boolean $debug 
 		 * @return [type]         
 		 */
-		public function delete($table,$where='',$debug=false){
+		public function delete($table,$where=''){
 			if($where == ''){
 				$this->outputError('where is Null');
 			}else{
-				$strSql = 'delete from '.$table.' where '.$where;
-				$result = $this->execSql($strSql,$debug);
+				$strSql = 'delete from `'.$table.'` where '.$where;
+				$result = $this->execSql($strSql);
 				return $result;
 			}
 		} 
 		/**
 		 * [execSql 执行sql]
 		 * @param  [type]  $strSql 
-		 * @param  boolean $debug  
 		 * @return [type]   
 		 */
-		public function execSql($strSql,$debug=false){
-			if($debug==true) $this->debug($strSql);
+		public function execSql($strSql){
+			if($this->debug==true) return $strSql;
 			$result = $this->dbh->exec($strSql);
 			$this->getPDOError();
 			return $result;
@@ -144,14 +168,13 @@
 		 * [getMaxValue 获取最大值]
 		 * @param  [type]  $table     
 		 * @param  [type]  $fields_name 
-		 * @param  string  $where      
-		 * @param  boolean $debug     
+		 * @param  string  $where          
 		 * @return [type]               
 		 */
-		public function getMaxValue($table,$fields_name,$where='',$debug=false){
-			$strSql = 'select max('.$fields_name.') as MAX_VALUE from '.$table;
+		public function getMaxValue($table,$fields_name,$where=''){
+			$strSql = 'select max('.$fields_name.') as MAX_VALUE from `'.$table.'`';
 			if($where!='') $strSql.=' where '.$where;
-			if($debug==true) $this->debug($strSql);
+			if($this->debug==true) return $strSql;
 			$arrTemp = $this->query($strSql,'Row');
 			$maxValue = $arrTemp['MAX_VALUE'];
 			if($maxValue==''||$maxValue==Null){
@@ -160,10 +183,10 @@
 			return $maxValue;
 		}
 		/*获取字段总数*/
-		public function getCount($table,$fields_name,$where='',$debug=false){
-			$strSql = 'select count('.$fields_name.') as NUM from '.$table;
+		public function getCount($table,$fields_name,$where=''){
+			$strSql = 'select count('.$fields_name.') as NUM from `'.$table.'`';
 			if($where!='') $strSql.=' where '.$where;
-			if($debug==true) $this->debug($strSql);
+			if($this->debug) return $strSql;
 			$arrTemp = $this->query($strSql,'Row');
 			return $arrTemp['NUM'];
 		}
@@ -208,6 +231,7 @@
 		 */
 		private function checkFields($table,$arrayFields){
 			$fields = $this->getFields($table);
+			if($this->debug==true) return $fields;
 			foreach ($arrayFields as $key => $value) {
 				if(!in_array($key,$fields)){
 					$this->outputError('unkown columns'.$key.' in fields list');
@@ -221,13 +245,17 @@
 		 */
 		private function getFields($table){
 			$fields = array();
-			$recordset = $this->dbh->query('show columns from '.$table);
+			$strSql = 'show columns from `'.$table.'`';
+			if($this->debug==true) return $strSql;
+			$recordset = $this->dbh->query($strSql);
 			$this->getPDOError();
 			$recordset->setFetchMode(PDO::FETCH_ASSOC);
 			$result = $recordset->fetchAll();
+
 			foreach ($result as $a) {
 				$fields[]=$a['Field'];
 			}
+
 			return $fields;
 		}
 		/**
@@ -249,66 +277,12 @@
     {
         throw new Exception('MySQL Error: '.$strErrMsg);
     }
-		/**
-		 * debug sql语句错误信息
-		 * @param mixed $debuginfo
-		 */
-		private function debug($debuginfo){
-			var_dump($debuginfo);
-			exit();
-		}
-		/**
-		 * [destruct 关闭数据库连接]
-		 * 
-		 */
-		public function destruct(){
-			$this->dbh = null;
-		}
 	/**
-	* 
-	* 和Es对应的查询索引
-	* @param $like 字符串 topology_%
-	* % 代表多个字符 _ 代码单个字符
-	* @param $return * topology_edg_ 
-	*/
-	public function getTables(string $like){
-		$ret = [];
-		$strSql = 'show tables like "'.$like.'"';
-        $arrayTableInfo=$this->query($strSql);
-        foreach($arrayTableInfo as $arr) $ret[] = array_shift($arr);
-        return $ret;
+	 * [destruct 关闭数据库连接]
+	 * 
+	 */
+	public function destruct(){
+		$this->dbh = null;
 	}
-
-	/**
-	* 批量查询
-	* @param $tables Array 一维数组，存储表名称
-	* @param $fields String 查询字段
-	* @param $hidden_table Bool 隐藏数据表名称
-	*/
-	public function batchQuery(Array $tables,String $fields = '*',$hidden_table=false){
-		$ret = [];
-		foreach($tables as $table){
-			$strSql = "select {$fields} from {$table}";
-			$cur = $this->query($strSql);
-
-			if($hidden_table){
-
-				foreach ($cur as $arr) $ret[] = $arr;					
-
-			}else{
-				
-				$ret[$table] = $cur;
-			
-			}
-
-		}
-
-		return $ret;
-
-	}		
-		
-	}
+}
 ?>
-
-
-
